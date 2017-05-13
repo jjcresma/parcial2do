8120dd61d62b73899f84d66f3ce976bc9c6d3cf2
jjcresma <josejuniorcr_1992@outlook.com>
2017-05-13 11:24:57 -0400

    archivos del examen

---
+++ tablaSuma.js
@@ -0,0 +1,24 @@
+
+var y = 7;
+var x= y;
+console.log("la suma de numeros 7:");
+console.log(y+"+1="+(++x));
+console.log(y+"+2="+(++x));
+console.log(y+"+3="+(++x));s
+console.log(y+"+4="+(++x));
+console.log(y+"+5="+(++x));
+console.log(y+"+6="+(++x));
+console.log(y+"+7="+(++x));
+console.log(y+"+8="+(++x));
+console.log(y+"+9="+(++x));
+console.log(y+"+10="+(++x));
+/*
+var suma= 0;
+var valor;
+while (x<=10)  {valor=prompt('ingrese valor:');
+valor = parseInt(valor);
+suma = suma + valor;
+x =
+};
+     
+console.log("la suma es:");*/
